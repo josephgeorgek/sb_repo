9a38a62a6c125eecc71af5798e945f904d5f5167
Joseph <josephgeorgek@gmail.com>
2021-03-30 16:30:34 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { Provider } from 'react-redux';
+import store from './lib/redux';
+
+import InboxScreen from './components/InboxScreen';
+
+
+function App() {
+  return (
+    <div className="App">
+  <Provider store={store}>
+      <InboxScreen />
+    </Provider>
+      
+    </div>
+  );
+}
+
+export default App;
